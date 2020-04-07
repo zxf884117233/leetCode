11a022650570adc5cdbebfc9008cb791981c1721
zhangxiaofeng <18734309079@163.com>
2020-04-08 00:41:53 +0800

    CombinationSum组合求和，利用回溯算法dfs求解

---
+++ src/leetCode/array/CombinationSum.java
@@ -0,0 +1,66 @@
+package leetCode.array;
+
+import javax.xml.xpath.XPath;
+import java.util.*;
+
+/**
+ * 求无重复数组中元素和为target的集合，元素可重复利用
+ * 思路：回溯算法dfs+剪枝的过程
+ * Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+ * <p>
+ * The same repeated number may be chosen from candidates unlimited number of times.
+ * <p>
+ * Note:
+ * <p>
+ * All numbers (including target) will be positive integers.
+ * The solution set must not contain duplicate combinations.
+ * Example 1:
+ * <p>
+ * Input: candidates = [2,3,6,7], target = 7,
+ * A solution set is:
+ * [
+ * [7],
+ * [2,2,3]
+ * ]
+ * Example 2:
+ * <p>
+ * Input: candidates = [2,3,5], target = 8,
+ * A solution set is:
+ * [
+ *   [2,2,2,2],
+ *   [2,3,3],
+ *   [3,5]
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/combination-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> lists = new ArrayList<>();
+        Arrays.sort(candidates);
+        dfs(candidates, candidates.length, target, 0, new ArrayDeque<>(), lists);
+        return lists;
+    }
+
+    private void dfs(int[] candidates, int length, int target, int begin, Deque<Integer> path, List<List<Integer>> lists) {
+        if (target == 0) {
+            lists.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = begin; i < length; i++) {
+            if (target - candidates[i] < 0) {
+                break;
+            }
+            path.addLast(candidates[i]);
+            dfs(candidates, length, target - candidates[i], i, path, lists);
+            path.removeLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        CombinationSum combinationSum = new CombinationSum();
+        System.out.println(combinationSum.combinationSum(new int[]{2, 3, 5}, 8));
+    }
+}
